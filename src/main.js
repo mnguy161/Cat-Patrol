b7b7ef6a64e51f487d2e93baa33a33fb51a69999
mnguy161 <mnguy161@ucsc.edu>
2020-04-18 04:00:27 -0700

    First version of the mod; decreasing timer added

---
+++ src/main.js
@@ -0,0 +1,19 @@
+let config = {
+    type: Phaser.CANVAS, 
+    width: 640, 
+    height: 480,
+    backgroundColor: 'rgba(209,239,234)',
+    scene: [ Menu, Play ], 
+};
+
+let game = new Phaser.Game(config); 
+
+//define game settings 
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
+
+//reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT;
+
